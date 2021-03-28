9b5749b2535ab3f7743cd73215a47eb47f72cfe9
DESKTOP-ENMOEDB\Toni <skuezyt@gmail.com>
2021-03-28 05:07:57 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+var cors = require("cors");
+const knex = require("knex");
+/* const { response } = require("express"); */
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "",
+    database: "smartbrain",
+  },
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+/* app.get("/", (req, res) => {
+  res.send(database.users);
+}); */
+
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", register.handleRegister(db, bcrypt));
+app.get("/profile/:id", profile.handleGetProfile(db));
+app.put("/image", image.handleImage(db));
+app.post("/imageurl", image.handleApiCall);
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
